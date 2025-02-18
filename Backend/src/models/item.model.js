25da1e72ba75f62167ae924589bebb440baf1b0a
shreyash <shreyashdelmade4@gmail.com>
2025-02-19 01:00:41 +0530

    feat : user,item,order models done

--- Backend/src/models/item.model.js
+++ Backend/src/models/item.model.js
@@ -1,5 +1,38 @@
-import { Schema } from "mongoose";
+
+import mongoose, { Schema } from "mongoose";
 
 const itemSchema = new Schema({
-    
-})
+    name:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    category: {
+        type: String,
+        enum: ["Electronics", "Furniture", "Vehicles", "Tools", "Clothing", "Others"],
+        required: true
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    price:{
+        type:Number,
+        required:true
+    },
+    available: {
+        type: Boolean,
+        default: true
+    },
+    images: {
+        type: [String], // Array of image URLs
+        default: []
+    }
+
+},{timestamps:true})
+
+export const Item = mongoose.model("Item",itemSchema)
