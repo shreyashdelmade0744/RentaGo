7fda469187dce694c6bb68af9018aec5d32caf1d
Shiv <shivpandey762@gmail.com>
2025-02-19 00:16:37 +0530

    chore: Signup Layout Added

---
+++ RentaGo/app/src/main/java/com/example/rentago/Signup/Signup.kt
@@ -0,0 +1,13 @@
+package com.example.rentago.Signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.rentago.R
+
+class Signup : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+
+    }
+}
